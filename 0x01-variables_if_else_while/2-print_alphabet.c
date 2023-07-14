abbf0bdcf079d0fdb4388c1a3a8483efcfc41133
Hajar <hajarzineeddine5@gmail.com>
2023-07-14 14:16:22 -0700

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar('a');
+	}
+	putchar('\n');
+	return (0);
+}
